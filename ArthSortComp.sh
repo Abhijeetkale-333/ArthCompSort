e1633888d793399a277c9830c2215937154e60e7
Your Name <you@example.com>
2021-03-25 14:55:47 +0530

    Added:ComputeUC3

--- ArthSortComp.sh
+++ ArthSortComp.sh
@@ -8,4 +8,11 @@ read b
 echo "Enter 3rd Input:"
 read c
 
+#compute
+
+operation1=`expr $a + $b \* $c`
+operation2=`expr $a \* $b + $c`
+
 echo "Three inputs are :" $a $b $c
+echo "Operation1:" $operation1
+echo "Operation2:" $operation2
